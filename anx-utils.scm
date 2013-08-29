2166ebc4fbb2aa18644e6e5ad0e6c1dc88cbd3d1
Rich Loveland <rloveland@appnexus.com>
2013-08-29 11:10:34 -0400

    Add small utilities file. Scheme always needs this.

---
+++ anx-utils.scm
@@ -0,0 +1,28 @@
+;;;; -*- mode: scheme48; scheme48-package: anx-utils -*-
+;;;; anx-utils.scm --- Various utilities.
+
+(define-structure anx-utils
+    (export
+     (push! :syntax)
+     (pop! :syntax)
+     )
+  (open 
+   scheme
+   scsh
+   )
+  (begin
+
+(define-syntax push!
+  (syntax-rules ()
+    ((push! item seq)
+     (begin (set! seq (cons item seq))
+            seq))))
+
+(define-syntax pop!
+  (syntax-rules ()
+    ((pop! seq)
+     (let ((result (car seq)))
+       (begin (set! seq (cdr seq))
+              result)))))
+
+))
