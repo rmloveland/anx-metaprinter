0b4b0e8f8a8738856e9b49b959e4fc5ccdb18cf2
Rich Loveland <rloveland@appnexus.com>
2014-02-06 09:27:41 -0500

    Add a few convenient exports to the ANX-DOCGEN package.

--- anx-docgen.scm
+++ anx-docgen.scm
@@ -8,7 +8,13 @@
 (define-interface anx-docgen-interface
   (export main				;defined in metaprinter.scm
 	  anx/really-print-meta
-	  anx/really-print-report-meta))
+	  anx/really-print-report-meta
+	  get-service-meta
+	  get-report-meta
+	  anx/extract-report-meta-fields
+	  anx/extract-meta-fields
+	  vector-map
+	  vector-for-each))
 
 (define-structure anx-docgen anx-docgen-interface
   (open
